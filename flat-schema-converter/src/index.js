70e77f7a8cd284bdebd18b6b8b6caeaa379a61e1
Carson Cook <carson.cook@ibm.com>
2022-02-10 11:01:38 -0500

    Clean up
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

--- flat-schema-converter/src/index.js
+++ flat-schema-converter/src/index.js
@@ -5,36 +5,27 @@ const nestedSchema = require("../zowe-schema.json");
 
 // validate nested schema before trying to convert
 ajv.compile(nestedSchema);
-
-printSchema(nestedSchema, "NESTED SCHEMA");
-
 const { properties, ...flatSchema } = nestedSchema;
 
 // resolve $ref references so they can be flattened
 const resolvedProperties = resolveReferences(properties, nestedSchema);
-printSchema(resolvedProperties, "RESOLVED");
-printSchema(properties, "ORIGINAL")
+
+const flatProperties = flattenProperties(resolvedProperties);
+flatSchema.properties = flatProperties;
+
+printSchema(flatSchema, "FLATTENED");
 
 function resolveReferences(properties, schema) {
     let resolvedProperties = {};
     for (const [k, prop] of (Object.entries(properties))) {
-        console.log(k);
         if (prop.properties) {
-            const r = resolveReferences(prop.properties, schema);
-            printSchema(r, "RECUR RESULT")
-            const res = { ...prop.properties, ...r };
-            printSchema(res, "RECUR BUILT RESULT");
-            resolvedProperties = { ...resolvedProperties, ...res };
-            printSchema(resolvedProperties, "RECURSION");
+            const resolved = { ...prop.properties, ...resolveReferences(prop.properties, schema) };
+            resolvedProperties = { ...resolvedProperties, ...resolved };
         }
 
         if (prop["$ref"]) {
-            const r = getReferencedObject(prop["$ref"], schema);
-            printSchema(r, "REFERENCE")
-            const resolvedProp = {...prop, ...r}
-            printSchema(resolvedProp, "RESOLVED PROP")
-            resolvedProperties[k] = { ...resolvedProperties[k], ...resolvedProp };
-            printSchema(resolvedProperties, "ref")
+            const resolved = {...prop, ...getReferencedObject(prop["$ref"], schema)}
+            resolvedProperties[k] = { ...resolvedProperties[k], ...resolved };
         }
     }
     return resolvedProperties;
@@ -57,11 +48,6 @@ function getReferencedObject(link, schema) {
 
     return resolvedObject;
 }
-
-const flatProperties = flattenProperties(resolvedProperties);
-flatSchema.properties = flatProperties;
-
-printSchema(flatSchema, "FLAT SCHEMA");
 
 function flattenProperties(properties, parentKey = "") {
     let flatProperties = {};
