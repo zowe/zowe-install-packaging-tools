955e343db88561086b3dc7e210d8531437a92554
Carson Cook <carson.cook@ibm.com>
2022-02-10 10:59:40 -0500

    Resolve  fields so they can be flattened
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

--- flat-schema-converter/src/index.js
+++ flat-schema-converter/src/index.js
@@ -9,28 +9,75 @@ ajv.compile(nestedSchema);
 printSchema(nestedSchema, "NESTED SCHEMA");
 
 const { properties, ...flatSchema } = nestedSchema;
-const flatProperties = flattenProperties(properties);
 
+// resolve $ref references so they can be flattened
+const resolvedProperties = resolveReferences(properties, nestedSchema);
+printSchema(resolvedProperties, "RESOLVED");
+printSchema(properties, "ORIGINAL")
 
+function resolveReferences(properties, schema) {
+    let resolvedProperties = {};
+    for (const [k, prop] of (Object.entries(properties))) {
+        console.log(k);
+        if (prop.properties) {
+            const r = resolveReferences(prop.properties, schema);
+            printSchema(r, "RECUR RESULT")
+            const res = { ...prop.properties, ...r };
+            printSchema(res, "RECUR BUILT RESULT");
+            resolvedProperties = { ...resolvedProperties, ...res };
+            printSchema(resolvedProperties, "RECURSION");
+        }
+
+        if (prop["$ref"]) {
+            const r = getReferencedObject(prop["$ref"], schema);
+            printSchema(r, "REFERENCE")
+            const resolvedProp = {...prop, ...r}
+            printSchema(resolvedProp, "RESOLVED PROP")
+            resolvedProperties[k] = { ...resolvedProperties[k], ...resolvedProp };
+            printSchema(resolvedProperties, "ref")
+        }
+    }
+    return resolvedProperties;
+}
+
+function getReferencedObject(link, schema) {
+    const resolvePath = link.split('/');
+    if (resolvePath[0] === '#') {
+        resolvePath.shift();
+    }
+
+    let resolvedObject = Object.assign({}, schema);
+    for (const p of resolvePath) {
+        if (resolvedObject[p]) {
+            resolvedObject = resolvedObject[p];
+        } else {
+            return null;
+        }
+    }
+
+    return resolvedObject;
+}
+
+const flatProperties = flattenProperties(resolvedProperties);
 flatSchema.properties = flatProperties;
 
 printSchema(flatSchema, "FLAT SCHEMA");
 
 function flattenProperties(properties, parentKey = "") {
-    let flatProperties = {}
+    let flatProperties = {};
     for (const [propKey, prop] of Object.entries(properties)) {
-        const keyPrefix = parentKey ? `${parentKey}.` : "" // ensure no leading .
+        const keyPrefix = parentKey ? `${parentKey}.` : "" // ensure no leading '.'
         if (prop.type === "object") {
-            flatProperties = {...flatProperties, ...flattenProperties(prop.properties, `${keyPrefix}${propKey}`) };
+            flatProperties = { ...flatProperties, ...flattenProperties(prop.properties, `${keyPrefix}${propKey}`) };
         } else {
-            flatProperties = {...flatProperties, [`${keyPrefix}${propKey}`]: prop }
+            flatProperties = { ...flatProperties, [`${keyPrefix}${propKey}`]: prop };
         }
     }
     return flatProperties;
 }
 
 function printSchema(schema, name = "") {
-    console.log(`~~~~~~~~~~~~~~~${name ? ' ' + name + ' ' : '~~'}~~~~~~~~~~~~~~~~`);
+    console.log(`~~~~~~~~~~~~~~~${name}~~~~~~~~~~~~~~~~`);
     console.log(JSON.stringify(schema, null, 4));
     console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 }
